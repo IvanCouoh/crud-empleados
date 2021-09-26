b8f1d4a052640180e27f34dc2eedc6f007622ffa
AngelGarridoP <ang31garr1d0@gmail.com>
2021-09-26 17:44:02 -0500

    modificacion-control

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       <div class="container">
           <div class="row">
               <div class="col-12">
-                  Página pricipal
+                  Página pricipal utilizando git flow
               </div>
           </div>
       </div>
