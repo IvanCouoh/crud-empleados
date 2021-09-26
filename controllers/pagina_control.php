f02432e047584b192097a195a242283c1ca0926f
AngelGarridoP <ang31garr1d0@gmail.com>
2021-09-26 18:25:49 -0500

    preparando-aplicacion
    creando el archivo inicio y error en la carpeta pages, agregando la carpeta controller con el primer controlador

---
+++ controllers/pagina_control.php
@@ -0,0 +1,12 @@
+<?php
+
+class paginaControl{
+    public function inicio(){
+        include_once("views/pages/inicio.php");
+    }
+    publicf function error(){
+        include_once("views/pages/error.php");
+    }
+}
+
+?>
