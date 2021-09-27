89778575f8d74b53bc81c283fee7bfdeb51fcab6
IvanCouoh <javier.ivancouoh@gmail.com>
2021-09-27 15:50:13 -0500

    Crear conexión a la base de datos

---
+++ conexion.php
@@ -0,0 +1,22 @@
+<?php
+
+
+    class conexion{
+        private static $instancia=NULL;
+
+        public static function crearInstancia(){
+            if(!isset(self::$instancia)){
+                $opcionesPDO[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+                //Conexión del servidor - Descomentar si se va usar o comentarlo si no
+                /* self::$instancia = new PDO('mysql:host=localhost;dbname=id17666837_empleados', 'id17666837_javier', 'UuWeSoa1~]6/*s4J', $opcionesPDO); */
+                
+                //Conexión local - Descomentar si se va usar o comentarlo si no
+                self::$instancia = new PDO('mysql:host=localhost;dbname=empleados', 'root', '', $opcionesPDO);
+                
+                echo "Conexión exitosa";
+            }
+            return self::$instancia;
+        }
+    }
+
+?>
