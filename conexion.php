d56c11126bc17a8a5def0c7f6d238d87d7f3cf62
IvanCouoh <javier.ivancouoh@gmail.com>
2021-09-30 13:09:36 -0500

    Conexion a bd posttgresql

--- conexion.php
+++ conexion.php
@@ -1,21 +1,27 @@
 <?php
 
 
-    class conexion{
-        private static $instancia=NULL;
+  class conexion{
+    private static $instancia=NULL;
+    public static function crearInstancia(){
 
-        public static function crearInstancia(){
-            if(!isset(self::$instancia)){
-                $opcionesPDO[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-                //Conexión del servidor - Descomentar si se va usar o comentarlo si no
-                self::$instancia = new PDO('pgsql:host=ec2-34-233-187-36.compute-1.amazonaws.com;dbname=ddo0bpjqjsglth', 'nypxktxwsrhlsd', '93a5dca7ec63568b7bb53794cf776a699070d6b6468e9f70eb06c5c5f8a369d1', $opcionesPDO); 
-                
-                //Conexión local - Descomentar si se va usar o comentarlo si no
-                // self::$instancia = new PDO('pgsql:host=localhost;dbname=empleados', 'root', '', $opcionesPDO);
-                
-            }
-            return self::$instancia;
-        }
+      $contraseña = "94c6227b24adf1b10eb41688c7bf1de3bcf2c0b0a6de1663413dbf30288c64da";
+      $usuario = "fepkmiqjprcoyn";
+      $nombreBaseDeDatos = "dmi6jhcel19ej";
+      $rutaServidor = "ec2-54-161-189-150.compute-1.amazonaws.com";
+      $puerto = "5432";
+
+      if(!isset(self::$instancia)){
+        $opcionesPDO[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        //Conexión del servidor - Descomentar si se va usar o comentarlo si no
+        self::$instancia = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
+        
+        //Conexión local - Descomentar si se va usar o comentarlo si no
+        // self::$instancia = new PDO('mysql:host=localhost;dbname=empleados', 'root', '', $opcionesPDO);
+          
+      }
+      return self::$instancia;
     }
+  }
 
 ?>
--- views/empleados/inicio.php
+++ views/empleados/inicio.php
@@ -3,44 +3,39 @@
     <a name="" id="" class="btn btn-primary" href="?controller=empleados&action=crear" role="button">Agregar empleado</a>
     </div>
     <div class="card-body">
-    <table class="table">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>NOMBRE</th>
-            <th>CORREO</th>
-            <th>FECHA INGRESO</th>
-            <th>ACCIONES</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($empleados as $empleado) { ?>
-        <tr>
-            <td> <?php echo $empleado->id;?> </td>
-            <td> <?php echo $empleado->nombre;?> </td>
-            <td> <?php echo $empleado->correo;?> </td>
-            <td> <?php echo $empleado->fecha;?></td>
-            
-            <td>
-            <div class="btn-group" role="group" aria-label="">
-                <a href="?controller=empleados&action=editar&id=<?php echo $empleado->id;?>" class="btn btn-warning" id=edit style= margin-left="2px">Editar</a>
- 
-                <a href="?controller=empleados&action=borrar&id=<?php echo $empleado->id;?>" class="btn btn-danger">Borrar</a>
-
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NOMBRE</th>
+                    <th>CORREO</th>
+                    <th>FECHA INGRESO</th>
+                    <th>ACCIONES</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($empleados as $empleado) { ?>
+                    <tr>
+                        <td> <?php echo $empleado->id;?> </td>
+                        <td> <?php echo $empleado->nombre;?> </td>
+                        <td> <?php echo $empleado->correo;?> </td>
+                        <td> <?php echo $empleado->fecha;?></td>
+                        
+                        <td>
+                            <div class="btn-group" role="group" aria-label="">
+                                <a href="?controller=empleados&action=editar&id=<?php echo $empleado->id;?>" class="btn btn-warning" id=edit style= margin-left="2px">Editar</a>
                 
-            </div>
-            </td>
-        </tr>
-        <?php } ?>
-    </tbody>
-</table>
+                                <a href="?controller=empleados&action=borrar&id=<?php echo $empleado->id;?>" class="btn btn-danger">Borrar</a>
+
+                                
+                            </div>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
 
 
     </div>
     
 </div>
-
-
-
-
-<tr>
