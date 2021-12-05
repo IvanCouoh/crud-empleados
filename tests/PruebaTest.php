0ea91610ff5889c21084adcd1b493f111d10ff5d
IvanCouoh <javier.ivancouoh@gmail.com>
2021-12-05 00:53:49 -0600

    test incorrecto

--- tests/PruebaTest.php
+++ tests/PruebaTest.php
@@ -14,9 +14,9 @@ class PruebaTest extends TestCase
         // $user->email('javiergmail.com');
 
         //valita la existencia de un correo
-        $this->assertEquals($user->setEmail(email:'javier@gmail.com'), 'javier@gmail.com');
+        $this->assertEquals($user->setEmail(email:'javiergmail.com'), 'javiergmail.com');
         
         //valita la existencia de un id
-        $this->assertEquals($user->setId(id:3), 3);
+        $this->assertEquals($user->setId(id:1), 1);
     }
 }
