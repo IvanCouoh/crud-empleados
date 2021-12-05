3d21ad5717c0f6fd01146a236c28784d6834e602
IvanCouoh <javier.ivancouoh@gmail.com>
2021-12-04 19:21:45 -0600

    Prueba test

--- tests/PruebaTest.php
+++ tests/PruebaTest.php
@@ -8,14 +8,17 @@ class PruebaTest extends TestCase {
     /** @test **/
     public function Comprobar_las_consultas_por_Http()
     {
+
+        $BDconexion= conexion::crearInstancia();
+
         $cliente = new GuzzleHttp\Client();
 
-        $res = $cliente->request('GET', 'https://jsonplaceholder.typicode.com/posts/1');
+        $res = $BDconexion->query("select * from empleados where id = 1");
 
         $data = json_decode($res -> getBody(), true);
 
-        $this -> assertArrayHasKey('userId', $data);
-        $this -> assertArrayHasKey('title', $data);
+        $this -> assertArrayHasKey('id', $data);
+        $this -> assertArrayHasKey('nombre', $data);
     }
 
     public function testFailure()
